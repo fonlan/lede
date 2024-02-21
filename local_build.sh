c64696fc8240adcb6022071256810883a9737387
fayang <fan-fonlan.yang@nokia-sbell.com>
2024-02-21 12:27:54 +0200

    Fix duplicate theme download and update config

--- local_build.sh
+++ local_build.sh
@@ -14,22 +14,18 @@ if [ -d package/lean/luci-app-poddns ]; then
 fi
 git clone https://github.com/fonlan/luci-app-poddns package/lean/luci-app-poddns
 
-find ./ | grep Makefile | grep luci-theme-argon | xargs rm -f
-if [ -d package/lean/luci-theme-argon ]; then
-    rm -rf package/lean/luci-theme-argon
-fi
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-if [ -d package/lean/luci-theme-design ]; then
-    rm -rf package/lean/luci-theme-design
-fi
-git clone https://github.com/gngpp/luci-theme-design.git package/lean/luci-theme-design
+# find ./ | grep Makefile | grep luci-theme-argon | xargs rm -f
+# if [ -d package/lean/luci-theme-argon ]; then
+#     rm -rf package/lean/luci-theme-argon
+# fi
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 if [ -d package/autoban ]; then
     rm -rf package/autoban
 fi
 git clone https://github.com/fonlan/autoban package/autoban
 
-#make menuconfig
 make defconfig
+make menuconfig
+
 make -j$(nproc) V=s
